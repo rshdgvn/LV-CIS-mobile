02fa2be83c959f390cd6868bc1c0ee68a3e98cbe
rshdgvn <rasheedgavinesponga@gmail.com>
2026-02-15 08:53:58 +0800

    fix: token key

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 import * as SecureStore from "expo-secure-store";
-import { API_URL } from "../utils/constant";
+import { API_URL, TOKEN_KEY } from "../utils/constant";
 
 export const api = axios.create({
   baseURL: API_URL,
@@ -11,7 +11,7 @@ export const api = axios.create({
 });
 
 api.interceptors.request.use(async (config) => {
-  const token = await SecureStore.getItemAsync("sanctum_token");
+  const token = await SecureStore.getItemAsync(TOKEN_KEY);
   if (token) {
     config.headers.Authorization = `Bearer ${token}`;
   }
