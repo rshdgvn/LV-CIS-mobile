08307bc7deac19b315a41fe72f8ca9d887fc2082
rshdgvn <rasheedgavinesponga@gmail.com>
2026-03-06 04:21:21 +0800

    feat(auth): add 401 response interceptor

--- src/api/api.ts
+++ src/api/api.ts
@@ -3,12 +3,18 @@ import * as SecureStore from "expo-secure-store";
 import { API_URL, TOKEN_KEY } from "../utils/config";
 
 export const api = axios.create({
-  baseURL: API_URL,
+  baseURL: API_URL, 
   headers: {
     "Content-Type": "application/json",
     Accept: "application/json",
   },
 });
+
+let onUnauthorizedCallback: () => void = () => {};
+
+export const setUnauthorizedCallback = (callback: () => void) => {
+  onUnauthorizedCallback = callback;
+};
 
 api.interceptors.request.use(async (config) => {
   const token = await SecureStore.getItemAsync(TOKEN_KEY);
@@ -17,3 +23,18 @@ api.interceptors.request.use(async (config) => {
   }
   return config;
 });
+
+api.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  async (error) => {
+    if (error.response?.status === 401) {
+      console.warn("API 401: Token invalid or expired. Auto-logging out.");
+      await SecureStore.deleteItemAsync(TOKEN_KEY);
+      
+      onUnauthorizedCallback();
+    }
+    return Promise.reject(error);
+  }
+);
